e1890d79dccf13bd9d2aa61095ee0d077eb56ad5
Fuad Ismail <fuad1502@gmail.com>
2024-03-25 02:19:30 +0700

    Add comments to cvwrapper.h

--- cvwrapper/cvwrapper.h
+++ cvwrapper/cvwrapper.h
@@ -5,9 +5,22 @@
 extern "C" {
 #endif
 
+// toJpg reads the given image file bytes (`inputBytes`) of `inputSize` size,
+// and returns JPG encoded file bytes of the image. The output file
+// size is written to `outputSize` pointer.
 unsigned char *toJpg(unsigned char *inputBytes, int inputSize, int *outputSize);
+
+// resizeImage reads the given image file bytes (`inputBytes`) of `intputSize`
+// size, and returns the PNG encoded file bytes of the image after it's resized
+// to (`width` px, `height` px). The output file size is written to `outputSize`
+// pointer.
 unsigned char *resizeImage(unsigned char *inputBytes, int inputSize, int width,
                            int height, int *outputSize);
+
+// compressImage reads the given image file bytes (`inputBytes`) of `intputSize`
+// size, and returns the JPG encoded file bytes of the image with compression
+// quality `quality`. The output file size is written to `outputSize`
+// pointer.
 unsigned char *compressImage(unsigned char *inputBytes, int inputSize,
                              int quality, int *outputSize);
 
