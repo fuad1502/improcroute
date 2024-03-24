2c3293dfd0ed2bf630067cc3b922eb77844090a6
Fuad Ismail <fuad1502@gmail.com>
2024-03-25 02:40:43 +0700

    Add comments to errorreporter package

--- service/errorreporter/errorreporter.go
+++ service/errorreporter/errorreporter.go
@@ -6,10 +6,16 @@ import (
 	"net/http"
 )
 
+// ErrorReporter is a struct that handles both writing error information to the
+// HTTP response and logging the Error. `FuncName` gives the log output more
+// context on which function it failed."
 type ErrorReporter struct {
 	FuncName string
 }
 
+// Report writes an HTTP response with status code `status` and a JSON payload
+// body {Error: `err`}. It also writes the error information to log, with an
+// additional function context.
 func (reporter ErrorReporter) Report(w http.ResponseWriter, status int, err error) {
 	errorStruct := struct{ Error string }{Error: err.Error()}
 	b, _ := json.Marshal(&errorStruct)
