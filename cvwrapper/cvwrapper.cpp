b045897175576c5add83dc1730cbfbaabb8e9143
Fuad Ismail <fuad1502@gmail.com>
2024-03-24 20:55:14 +0700

    Apply clang tidy format

--- cvwrapper/cvwrapper.cpp
+++ cvwrapper/cvwrapper.cpp
@@ -5,41 +5,46 @@
 #include <opencv4/opencv2/imgcodecs.hpp>
 #include <opencv4/opencv2/imgproc.hpp>
 
-#ifdef __cplusplus 
+#ifdef __cplusplus
 extern "C" {
 #endif
 
-unsigned char* pngToJpg(unsigned char* inputBytes, int inputSize, int *outputSize) {
-	cv::InputArray inputArray{inputBytes, inputSize};
-	auto inputImage = cv::imdecode(inputArray, cv::IMREAD_UNCHANGED);
-	if (inputImage.data == NULL) {
-		return nullptr;
-	}
-	std::vector<uchar> outputVec;
-	cv::imencode(".jpg", inputImage, outputVec);
-	*outputSize = outputVec.size();
-	unsigned char *outputBytes = (unsigned char*) malloc(sizeof(unsigned char) * *outputSize);
-	memcpy((void*)outputBytes, (void*)outputVec.data(), sizeof(unsigned char) * *outputSize);
-	return outputBytes;
+unsigned char *pngToJpg(unsigned char *inputBytes, int inputSize,
+                        int *outputSize) {
+  cv::InputArray inputArray{inputBytes, inputSize};
+  auto inputImage = cv::imdecode(inputArray, cv::IMREAD_UNCHANGED);
+  if (inputImage.data == NULL) {
+    return nullptr;
+  }
+  std::vector<uchar> outputVec;
+  cv::imencode(".jpg", inputImage, outputVec);
+  *outputSize = outputVec.size();
+  unsigned char *outputBytes =
+      (unsigned char *)malloc(sizeof(unsigned char) * *outputSize);
+  memcpy((void *)outputBytes, (void *)outputVec.data(),
+         sizeof(unsigned char) * *outputSize);
+  return outputBytes;
 }
 
-unsigned char* resizeImage(unsigned char* inputBytes, int inputSize, int width, int height, int *outputSize) {
-	cv::InputArray inputArray{inputBytes, inputSize};
-	auto inputImage = cv::imdecode(inputArray, cv::IMREAD_UNCHANGED);
-	if (inputImage.data == NULL) {
-		return nullptr;
-	}
-	cv::Mat outputImage{};
-	cv::resize(inputImage, outputImage, cv::Size(width, height));
-	std::vector<uchar> outputVec;
-	cv::imencode(".jpg", outputImage, outputVec);
-	*outputSize = outputVec.size();
-	unsigned char *outputBytes = (unsigned char*) malloc(sizeof(unsigned char) * *outputSize);
-	memcpy((void*)outputBytes, (void*)outputVec.data(), sizeof(unsigned char) * *outputSize);
-	return outputBytes;
+unsigned char *resizeImage(unsigned char *inputBytes, int inputSize, int width,
+                           int height, int *outputSize) {
+  cv::InputArray inputArray{inputBytes, inputSize};
+  auto inputImage = cv::imdecode(inputArray, cv::IMREAD_UNCHANGED);
+  if (inputImage.data == NULL) {
+    return nullptr;
+  }
+  cv::Mat outputImage{};
+  cv::resize(inputImage, outputImage, cv::Size(width, height));
+  std::vector<uchar> outputVec;
+  cv::imencode(".jpg", outputImage, outputVec);
+  *outputSize = outputVec.size();
+  unsigned char *outputBytes =
+      (unsigned char *)malloc(sizeof(unsigned char) * *outputSize);
+  memcpy((void *)outputBytes, (void *)outputVec.data(),
+         sizeof(unsigned char) * *outputSize);
+  return outputBytes;
 }
 
-#ifdef __cplusplus 
+#ifdef __cplusplus
 }
 #endif
-
--- cvwrapper/cvwrapper.h
+++ cvwrapper/cvwrapper.h
@@ -1,14 +1,16 @@
 #ifndef CV_WRAPPER_H
 #define CV_WRAPPER_H
 
-#ifdef __cplusplus 
+#ifdef __cplusplus
 extern "C" {
 #endif
 
-unsigned char* pngToJpg(unsigned char* inputBytes, int inputSize, int *outputSize);
-unsigned char* resizeImage(unsigned char* inputBytes, int inputSize, int width, int height, int *outputSize);
+unsigned char *pngToJpg(unsigned char *inputBytes, int inputSize,
+                        int *outputSize);
+unsigned char *resizeImage(unsigned char *inputBytes, int inputSize, int width,
+                           int height, int *outputSize);
 
-#ifdef __cplusplus 
+#ifdef __cplusplus
 }
 #endif
 
